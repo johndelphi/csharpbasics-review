9ab65c0b5126f2dc582c03fa48bdad1f8d308d1d
John.michael <John.michael@studentambassadors.com>
2022-01-22 15:12:36 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+namespace csharpbasics_review
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      //linq queries and opperators
+      var users = new string[] { "jeff", "tom","john","mark"};
+      var userQuery = from user in users
+                      where user.Contains("m")
+                      orderby user.Length ascending
+                                            select user;
+      foreach(var user in users)
+      {
+                Console.WriteLine(userQuery);
+      }
+      
+    }
+  }
+}
