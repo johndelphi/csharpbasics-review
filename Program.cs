b5d293d3a5014f928e252e597a623552f403a42d
John.michael <John.michael@studentambassadors.com>
2022-01-24 01:11:13 -0600

    Changed query name

--- Program.cs
+++ Program.cs
@@ -7,14 +7,14 @@ namespace csharpbasics_review
     static void Main(string[] args)
     {
       //linq queries and opperators
-      var users = new string[] { "jeff", "tom","john","mark"};
-      var userQuery = from user in users
+      var usersQuery = new string[] { "jeff", "tom","john","mark"};
+      var userQuery = from user in usersQuery
                       where user.Contains("m")
                       orderby user.Length ascending
                                             select user;
-      foreach(var user in users)
+      foreach(var user in usersQuery)
       {
-                Console.WriteLine(userQuery);
+                Console.WriteLine(user);
       }
       
     }
